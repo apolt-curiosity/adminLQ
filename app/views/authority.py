fce6d2a67b27a11a3cc227a3e7357f2c992f7922
apoltcuriosity <apoltcuriosity@163.com>
2020-05-01 21:53:08 +0200

    用户登录初步

--- app/views/authority.py
+++ app/views/authority.py
@@ -1,7 +1,17 @@
-from flask import Blueprint,current_app,jsonify
+from time import time
+
+from flask import Blueprint
+from flask import jsonify
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask import current_app
+from flask import make_response
 from authlib.jose import jwt
 
-from ..models import AuthUser,_db_adminlq_connect,_db_adminlq_close
+from ..models import AuthUser
+from ..models import _db_adminlq_connect
+from ..models import _db_adminlq_close
 
 authority = Blueprint('authority', __name__, url_prefix='/admin/authority')
 
@@ -12,7 +22,7 @@ authority.teardown_request(_db_adminlq_close)
 # rsa_private_key = current_app.config['RSA_PRIVATE_KEY']
 
 @authority.route('/')
-def ttt():
+def get_cookie():
     auth_user = AuthUser()
     ret = {'1':1,'2':2}
     i = 1
@@ -24,16 +34,48 @@ def ttt():
             ret["2"] = user
             break
     
-    payload = {'iss': 'adminlq', 'sub': 'test', 'test':'kdj'}
+    
    
-    header = current_app.config['JWT_HEADER']
+    
     rsa_public_key = current_app.config['RSA_PUBLIC_KEY']
-    rsa_private_key = current_app.config['RSA_PRIVATE_KEY']
-    s = jwt.encode(header, payload, rsa_private_key)
+    
+    s = request.cookies.get('jwt')
     claims = jwt.decode(s, rsa_public_key)
     
     return jsonify(claims)
 
 @authority.route('/set')
-def set_jwt():
-    return 'setjwt'
+def set_cookie():
+    # cookie参数
+    path = current_app.config['SET_COOKIE_PATH']
+    domain = current_app.config['SET_COOKIE_DOMAIN']
+    secure = current_app.config['SET_COOKIE_SECURE']
+    httponly = current_app.config['SET_COOKIE_HTTPONLY']
+    samesite = current_app.config['SET_COOKIE_SAMESITE']
+    max_age = current_app.config['SET_COOKIE_SIX_DAYS']
+    expires = time() + max_age
+
+    # jwt参数
+    header = current_app.config['JWT_HEADER']
+    rsa_private_key = current_app.config['RSA_PRIVATE_KEY']
+
+    payload = {'iss': 'adminlq', 'sub': 'chq', 'user':'flq'}
+    jwt_value = jwt.encode(header, payload, rsa_private_key)
+
+    resp = make_response(redirect(url_for('authority.get_cookie')))
+
+    key = 'jwt'
+    value = jwt_value
+    resp.set_cookie(
+        key,
+        value=value,
+        max_age=max_age,
+        expires=expires,
+        path=path,
+        domain=domain,
+        secure=secure,
+        httponly=httponly,
+        samesite=samesite
+    )
+
+    return resp
