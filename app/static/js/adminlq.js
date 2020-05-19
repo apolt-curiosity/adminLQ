52b0b80ccb1c265ef217c2f2dec1cf1c1930ebba
apoltcuriosity <50551966+apolt-curiosity@users.noreply.github.com>
2020-05-19 22:49:46 +0200

    css动画测试

--- app/static/js/adminlq.js
+++ app/static/js/adminlq.js
@@ -1,44 +1,31 @@
-var f = document.getElementById('sidebar');
-        // document.addEventListener('click',function(){
-        //     if(window.getComputedStyle(f,null)['marginLeft']=='200px'){
-        //             f.style.marginLeft = 0 + 'px';
-        //         }
-        // });
-        // window.onresize = function(){
-        //     f.style.marginLeft ='0px';
-        // };
+window.onresize = function kk(){
+    if((window.innerWidth<=400)){
+        sidebar.classObject['sidebar-hidden'] = true
+    }else{
+        sidebar.classObject['sidebar-hidden'] = false
+    }
+}
+const sidebar = new Vue({
+    el: '#sidebar',
+    data: {
+        classObject: {
+            'sidebar-hidden': (window.innerWidth<=400)
+        }
+    }
 
-       
-        document.getElementById('mybutton').addEventListener('click',function(){
-            // document.getElementById('ddd').classList.add('animate__fadeOutLeft');
-            // document.getElementById('eee').classList.add('animate__fadeInRight');
-            // document.getElementById('ddd').classList.remove('is-3');
-            // if(window.getComputedStyle(f,null)['left']=='-200px'){
-            //     if(window.getComputedStyle(f,null)['marginLeft']=='200px'){
-            //         f.style.marginLeft = 0 + 'px';
-            //     }else{
-            //         f.style.marginLeft = 200 + 'px';
-            //     }
-                
-            // }
-            document.getElementById('sidebar').classList.add('sidebar-transtion');
-        });
+})
 
-// const Foo = { template: '<div>foo</div>' }
-// const Bar = { template: '<div>bar</div>' }
+const mybutton = new Vue({
+    el: '#mybutton',
+    methods: {
+        show: function(event){
+            
+            if((window.innerWidth<=400)){
+                alert('dd')
+                sidebar.classObject['sidebar-hidden'] = !sidebar.classObject['sidebar-hidden']
+            }
+            
+        }
+    }
+})
 
-
-// const routes = [
-//     { path: '/foo', component: {template:'#foo'} },
-//     { path: '/bar', component: Bar }
-// ]
-
-
-// const router = new VueRouter({
-//     routes:routes 
-// })
-
-
-// const app = new Vue({
-//     router
-// }).$mount('#app')
