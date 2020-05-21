c13513cd58d7950a2b71795ff7757a6831ff91d1
apoltcuriosity <50551966+apolt-curiosity@users.noreply.github.com>
2020-05-21 23:09:15 +0200

    外观修改

--- app/static/js/adminlq.js
+++ app/static/js/adminlq.js
@@ -1,41 +1,42 @@
 window.onresize = () => {
     if((window.innerWidth <= 1023)){
-        leftbar.classObject['adminlq-leftbar-hidden'] = true
+        sidebar.classObject['adminlq-sidebar-hidden'] = true;
     }else{
-        leftbar.classObject['adminlq-leftbar-hidden'] = false 
+        sidebar.classObject['adminlq-sidebar-hidden'] = false;
     }
+    adminlqSiderbarMask.styleObject['z-index'] = -15;
 }
-// const topbarContent = new Vue({
-//     el: '#topbar-blank',
-//     data: {
-//         classObject: {
-//             'topbar-content-margin': !(window.innerWidth <= 1023)
-//         }
-//     }
-
-// })
-
-const leftbar = new Vue({
-    el: '#adminlq-leftbar',
+const sidebar = new Vue({
+    el: '#adminlq-sidebar',
     data: {
         classObject: {
-            'adminlq-leftbar-hidden': (window.innerWidth <= 1023)
+            'adminlq-sidebar-hidden': (window.innerWidth <= 1023)
         }
     }
-
 })
-
-const mybutton = new Vue({
-    el: '#mybutton',
+const headerContentSidebarButton = new Vue({
+    el: '#header-content-sidebar-button',
     methods: {
-        show: function(event){
-            
-            if((window.innerWidth<=1023)){
-                alert('dd')
-                sidebar.classObject['sidebar-hidden'] = !sidebar.classObject['sidebar-hidden']
+        sidebarToggle: function(event){
+            if((window.innerWidth <= 1023)){
+                sidebar.classObject['adminlq-sidebar-hidden'] = !sidebar.classObject['adminlq-sidebar-hidden'];
+                adminlqSiderbarMask.styleObject['z-index'] = 10;
             }
             
         }
     }
 })
-
+const adminlqSiderbarMask = new Vue({
+    el: '#adminlq-siderbar-mask',
+    data: {
+        styleObject: {
+            'z-index': -15
+        }
+    },
+    methods: {
+        sidebarMaskClick: function(event){
+            sidebar.classObject['adminlq-sidebar-hidden'] = true;
+            this.styleObject['z-index'] = -15;
+        }
+    }
+})
