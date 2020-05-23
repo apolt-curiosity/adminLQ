4123beb81f12e264fd77c4d1d85430e5a7dbe037
apoltcuriosity <50551966+apolt-curiosity@users.noreply.github.com>
2020-05-24 00:03:42 +0200

    vue组件排坑

--- app/static/js/adminlq.js
+++ app/static/js/adminlq.js
@@ -6,14 +6,6 @@ window.onresize = () => {
     }
     adminlqSiderbarMask.styleObject['z-index'] = -15;
 }
-const sidebar = new Vue({
-    el: '#adminlq-sidebar',
-    data: {
-        classObject: {
-            'adminlq-sidebar-hidden': (window.innerWidth <= 1023)
-        }
-    }
-})
 const headerContentSidebarButton = new Vue({
     el: '#header-content-sidebar-button',
     methods: {
@@ -26,6 +18,32 @@ const headerContentSidebarButton = new Vue({
         }
     }
 })
+const sidebar = new Vue({
+    el: '#adminlq-sidebar',
+    data: {
+        aa:true,
+        bb:{
+            'padding-top': '20px'
+        },
+        classObject: {
+            'adminlq-sidebar-hidden': (window.innerWidth <= 1023)
+        }
+    }
+})
+
+
+
+// const sidebarHeaderContent = new Vue({
+//     el: '#sidebar-header-content',
+//     data: {
+//         aa:true
+//     },
+//     methods:{
+//         aaa: function(event){
+//             alert('222')
+//         }
+//     }
+// })
 const adminlqSiderbarMask = new Vue({
     el: '#adminlq-siderbar-mask',
     data: {
