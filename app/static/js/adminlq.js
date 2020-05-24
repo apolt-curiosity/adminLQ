19b4816e645e185f76bfef68469f0a8283eb9ed2
apoltcuriosity <50551966+apolt-curiosity@users.noreply.github.com>
2020-05-24 23:48:20 +0200

    修改组件结构

--- app/static/js/adminlq.js
+++ app/static/js/adminlq.js
@@ -1,60 +1,62 @@
-window.onresize = () => {
-    if((window.innerWidth <= 1023)){
-        sidebar.classObject['adminlq-sidebar-hidden'] = true;
-    }else{
-        sidebar.classObject['adminlq-sidebar-hidden'] = false;
-    }
-    adminlqSiderbarMask.styleObject['z-index'] = -15;
-}
-const headerContentSidebarButton = new Vue({
-    el: '#header-content-sidebar-button',
+// adminlq.js v0.1.1
+const TOPBAR_HEIGHT = '50px';
+const TOPBAR_Z_INDEX = 5;
+const SIDEBAR_WIDTH = '180px';
+const SIDEBAR_Z_INDEX = 15;
+const MASK_FOR_SIDEBAR_SHOW_Z_INDEX = 10;
+const MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX = -15;
+const ADMINLQ_BACKGROUND_Z_INDEX = -5;
+const MOBILE_MAX_WIDTH = 768;
+const TABLET_MAX_WIDTH = 1023;
+const DESKTOP_MAX_WIDTH = 1215;
+const WIDESCREEN_MAX_WIDTH = 1407;
+const FULLHD_MIN_WIDTH = 1408;
+Vue.component('topbar-sidebar-toggle-button', {
+    template: '#topbar-sidebar-toggle-button-template',
     methods: {
-        sidebarToggle: function(event){
-            if((window.innerWidth <= 1023)){
-                sidebar.classObject['adminlq-sidebar-hidden'] = !sidebar.classObject['adminlq-sidebar-hidden'];
-                adminlqSiderbarMask.styleObject['z-index'] = 10;
+        topbarSidebarToggle: (event) => {
+            if(window.innerWidth <= TABLET_MAX_WIDTH){
+                adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
+                adminlqApp.maskForSidebarStyle['z-index'] = MASK_FOR_SIDEBAR_SHOW_Z_INDEX;
             }
-            
         }
     }
-})
-const sidebar = new Vue({
-    el: '#adminlq-sidebar',
+});
+const adminlqApp = new Vue({
+    el: '#adminlq-app',
     data: {
-        aa:true,
-        bb:{
-            'padding-top': '20px'
+        adminlqTopbarStyle: {
+            'height': TOPBAR_HEIGHT,
+            'z-index': TOPBAR_Z_INDEX
         },
-        classObject: {
-            'adminlq-sidebar-hidden': (window.innerWidth <= 1023)
-        }
-    }
-})
-
-
-
-// const sidebarHeaderContent = new Vue({
-//     el: '#sidebar-header-content',
-//     data: {
-//         aa:true
-//     },
-//     methods:{
-//         aaa: function(event){
-//             alert('222')
-//         }
-//     }
-// })
-const adminlqSiderbarMask = new Vue({
-    el: '#adminlq-siderbar-mask',
-    data: {
-        styleObject: {
-            'z-index': -15
+        topbarBlankStyle: {
+            'width': SIDEBAR_WIDTH,
+            'height': TOPBAR_HEIGHT
+        },
+        sidebarStyle: {
+            'width': SIDEBAR_WIDTH,
+            'z-index': SIDEBAR_Z_INDEX,
+            'transform': (window.innerWidth <= TABLET_MAX_WIDTH)?`translateX(-${SIDEBAR_WIDTH})`:'translateX(0px)'
+        },
+        maskForSidebarStyle: {
+            'z-index': MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX
+        },
+        adminlqBackgroundStyle:{
+            'z-index': ADMINLQ_BACKGROUND_Z_INDEX
         }
     },
     methods: {
-        sidebarMaskClick: function(event){
-            sidebar.classObject['adminlq-sidebar-hidden'] = true;
-            this.styleObject['z-index'] = -15;
+        maskForSidebarClick: (event) => {
+            adminlqApp.sidebarStyle['transform'] = `translateX(-${SIDEBAR_WIDTH})`;
+            adminlqApp.maskForSidebarStyle['z-index'] = MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX;
         }
     }
-})
+});
+window.onresize = () => {
+    if(window.innerWidth <= TABLET_MAX_WIDTH){
+        adminlqApp.sidebarStyle['transform'] = `translateX(-${SIDEBAR_WIDTH})`;
+    }else{
+        adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
+    }
+    adminlqApp.maskForSidebarStyle['z-index'] = MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX;
+};
