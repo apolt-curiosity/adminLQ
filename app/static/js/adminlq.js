83ed3be74c2c356dfbf7a7340867068a4a40936c
apoltcuriosity <50551966+apolt-curiosity@users.noreply.github.com>
2020-05-26 23:41:44 +0200

    vue组件修改

--- app/static/js/adminlq.js
+++ app/static/js/adminlq.js
@@ -1,86 +1,105 @@
-// adminlq.js v0.1.1
-const TOPBAR_HEIGHT = '50px';
-const TOPBAR_Z_INDEX = 5;
-const SIDEBAR_WIDTH = '180px';
-const SIDEBAR_WIDTH_ACCORDION = '60px';
-const SIDEBAR_WIDTH_HIDDEN = '0px';
-const SIDEBAR_Z_INDEX = 15;
-const MASK_FOR_SIDEBAR_SHOW_Z_INDEX = 10;
-const MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX = -15;
+/**
+  * adminlq.js v0.1.1
+  */
+const ADMINLQ_TOPBAR_HEIGHT = '50px';
+const ADMINLQ_TOPBAR_Z_INDEX = 5;
+const ADMINLQ_SIDEBAR_WIDTH = '200px';
+const ADMINLQ_SIDEBAR_WIDTH_ACCORDION = '60px';
+const ADMINLQ_SIDEBAR_WIDTH_HIDDEN = '0px';
+const ADMINLQ_SIDEBAR_Z_INDEX = 15;
+const ADMINLQ_MASK_FOR_SIDEBAR_SHOW_Z_INDEX = 10;
+const ADMINLQ_MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX = -15;
 const ADMINLQ_BACKGROUND_Z_INDEX = -5;
-const MOBILE_MAX_WIDTH = 768;
-const TABLET_MAX_WIDTH = 1023;
-const DESKTOP_MAX_WIDTH = 1215;
-const WIDESCREEN_MAX_WIDTH = 1407;
-const FULLHD_MIN_WIDTH = 1408;
-Vue.component('topbar-sidebar-toggle-button', {
-    template: '#topbar-sidebar-toggle-button-template',
-    methods: {
-        topbarSidebarToggle: (event) => {
-            if(window.innerWidth <= TABLET_MAX_WIDTH){
-                adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
-                adminlqApp.maskForSidebarStyle['z-index'] = MASK_FOR_SIDEBAR_SHOW_Z_INDEX;
-            }else{
-                if(adminlqApp.sidebar_accordion_state){
-                    adminlqApp.topbarBlankStyle['width'] = SIDEBAR_WIDTH;
-                    adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
-                    adminlqApp.sidebar_accordion_state = false;
-                }else{
-                    adminlqApp.topbarBlankStyle['width'] = SIDEBAR_WIDTH_ACCORDION;
-                    adminlqApp.sidebarStyle['transform'] = adminlqApp.sidebar_accordion_translatex;
-                    adminlqApp.sidebar_accordion_state = true;
-                }
-            }
-        }
-    }
-});
-const adminlqApp = new Vue({
+const ADMINLQ_MOBILE_MAX_WIDTH = 768;
+const ADMINLQ_TABLET_MAX_WIDTH = 1023;
+const ADMINLQ_DESKTOP_MAX_WIDTH = 1215;
+const ADMINLQ_WIDESCREEN_MAX_WIDTH = 1407;
+const ADMINLQ_FULLHD_MIN_WIDTH = 1408;
+
+const adminlqApp = new Vue ({
     el: '#adminlq-app',
     data: {
-        sidebar_hidden_state: window.innerWidth <= TABLET_MAX_WIDTH,
-        sidebar_accordion_state: true,
-        sidebar_accordion_translatex: `translateX(${(Number(SIDEBAR_WIDTH_ACCORDION.slice(0,(SIDEBAR_WIDTH_ACCORDION.length - 2))) - Number(SIDEBAR_WIDTH.slice(0,(SIDEBAR_WIDTH.length - 2)))).toString()}px)`,
+        sidebar_hidden_state: window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH,
+        sidebar_accordion_state: false,
+        sidebar_accordion_translatex: `translateX(${(Number(ADMINLQ_SIDEBAR_WIDTH_ACCORDION.slice(0,(ADMINLQ_SIDEBAR_WIDTH_ACCORDION.length - 2))) - Number(ADMINLQ_SIDEBAR_WIDTH.slice(0,(ADMINLQ_SIDEBAR_WIDTH.length - 2)))).toString()}px)`,
         adminlqTopbarStyle: {
-            'height': TOPBAR_HEIGHT,
-            'z-index': TOPBAR_Z_INDEX
+            'height': ADMINLQ_TOPBAR_HEIGHT,
+            'z-index': ADMINLQ_TOPBAR_Z_INDEX
         },
-        topbarBlankStyle: {
-            'width': (window.innerWidth <= TABLET_MAX_WIDTH)?SIDEBAR_WIDTH_HIDDEN:SIDEBAR_WIDTH,
-            'height': TOPBAR_HEIGHT
+        adminlqBlankStyle: {
+            'width': (window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH)?ADMINLQ_SIDEBAR_WIDTH_HIDDEN:ADMINLQ_SIDEBAR_WIDTH,
+        },
+        antIndentSvgIconClass: {
+            'is-hidden': !(window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH)
+        },
+        antOutdentSvgIconClass: {
+            'is-hidden': window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH
         },
         sidebarStyle: {
-            'width': SIDEBAR_WIDTH,
-            'z-index': SIDEBAR_Z_INDEX,
-            'transform': (window.innerWidth <= TABLET_MAX_WIDTH)?`translateX(-${SIDEBAR_WIDTH})`:'translateX(0px)'
+            'width': ADMINLQ_SIDEBAR_WIDTH,
+            'z-index': ADMINLQ_SIDEBAR_Z_INDEX,
+            'transform': (window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH)?`translateX(-${ADMINLQ_SIDEBAR_WIDTH})`:'translateX(0px)'
         },
         maskForSidebarStyle: {
-            'z-index': MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX
+            'z-index': ADMINLQ_MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX
         },
         adminlqBackgroundStyle:{
             'z-index': ADMINLQ_BACKGROUND_Z_INDEX
         }
     },
     methods: {
+        topbarSidebarToggle: (event) => {
+            if(window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH){
+                adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
+                adminlqApp.maskForSidebarStyle['z-index'] = ADMINLQ_MASK_FOR_SIDEBAR_SHOW_Z_INDEX;
+                adminlqApp.antIndentSvgIconClass['is-hidden'] = true;
+                adminlqApp.antOutdentSvgIconClass['is-hidden'] = false;
+                adminlqApp.sidebar_hidden_state = false;
+            }else{
+                if(adminlqApp.sidebar_accordion_state){
+                    adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
+                    adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
+                    adminlqApp.antIndentSvgIconClass['is-hidden'] = true;
+                    adminlqApp.antOutdentSvgIconClass['is-hidden'] = false;
+                    adminlqApp.sidebar_accordion_state = false;
+                }else{
+                    adminlqApp.sidebarStyle['transform'] = adminlqApp.sidebar_accordion_translatex;
+                    adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
+                    adminlqApp.antOutdentSvgIconClass['is-hidden'] = true;
+                    adminlqApp.antIndentSvgIconClass['is-hidden'] = false;
+                    adminlqApp.sidebar_accordion_state = true;
+                }
+            }
+        },
         maskForSidebarClick: (event) => {
-            adminlqApp.sidebarStyle['transform'] = `translateX(-${SIDEBAR_WIDTH})`;
-            adminlqApp.maskForSidebarStyle['z-index'] = MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX;
+            adminlqApp.sidebarStyle['transform'] = `translateX(-${ADMINLQ_SIDEBAR_WIDTH})`;
+            adminlqApp.maskForSidebarStyle['z-index'] = ADMINLQ_MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX;
+            adminlqApp.antOutdentSvgIconClass['is-hidden'] = true;
+            adminlqApp.antIndentSvgIconClass['is-hidden'] = false;
+            adminlqApp.sidebar_hidden_state = true;
         }
     }
 });
 window.onresize = () => {
-    if(window.innerWidth <= TABLET_MAX_WIDTH){
-        adminlqApp.sidebarStyle['transform'] = `translateX(-${SIDEBAR_WIDTH})`;
-        adminlqApp.topbarBlankStyle['width'] = SIDEBAR_WIDTH_HIDDEN;
+    if(window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH){
+        adminlqApp.sidebarStyle['transform'] = `translateX(-${ADMINLQ_SIDEBAR_WIDTH})`;
+        adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_HIDDEN;
+        adminlqApp.antOutdentSvgIconClass['is-hidden'] = true;
+        adminlqApp.antIndentSvgIconClass['is-hidden'] = false;
         adminlqApp.sidebar_hidden_state = true;
     }else{
         if(adminlqApp.sidebar_accordion_state){
-            adminlqApp.topbarBlankStyle['width'] = SIDEBAR_WIDTH_ACCORDION;
+            adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
             adminlqApp.sidebarStyle['transform'] = adminlqApp.sidebar_accordion_translatex;
+            adminlqApp.antOutdentSvgIconClass['is-hidden'] = true;
+            adminlqApp.antIndentSvgIconClass['is-hidden'] = false;
         }else{
-            adminlqApp.topbarBlankStyle['width'] = SIDEBAR_WIDTH;
+            adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
             adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
+            adminlqApp.antIndentSvgIconClass['is-hidden'] = true;
+            adminlqApp.antOutdentSvgIconClass['is-hidden'] = false;
         }
+        adminlqApp.sidebar_hidden_state = false;
     }
-    adminlqApp.maskForSidebarStyle['z-index'] = MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX;
-    adminlqApp.sidebar_hidden_state = false;
+    adminlqApp.maskForSidebarStyle['z-index'] = ADMINLQ_MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX;
 };
