33aef1970b113677a8b27c4c9cbfc4d52bdaa515
apoltcuriosity <50551966+apolt-curiosity@users.noreply.github.com>
2020-05-28 21:56:09 +0200

    随便改改

--- app/static/js/adminlq.js
+++ app/static/js/adminlq.js
@@ -15,30 +15,40 @@ const ADMINLQ_TABLET_MAX_WIDTH = 1023;
 const ADMINLQ_DESKTOP_MAX_WIDTH = 1215;
 const ADMINLQ_WIDESCREEN_MAX_WIDTH = 1407;
 const ADMINLQ_FULLHD_MIN_WIDTH = 1408;
+const ADMINLQ_APP_BREAKPOINT_WIDTH = ADMINLQ_TABLET_MAX_WIDTH;
 
 const adminlqApp = new Vue ({
     el: '#adminlq-app',
     data: {
-        sidebar_hidden_state: window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH,
+        sidebar_hidden_state: window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH,
         sidebar_accordion_state: false,
         sidebar_accordion_translatex: `translateX(${(Number(ADMINLQ_SIDEBAR_WIDTH_ACCORDION.slice(0,(ADMINLQ_SIDEBAR_WIDTH_ACCORDION.length - 2))) - Number(ADMINLQ_SIDEBAR_WIDTH.slice(0,(ADMINLQ_SIDEBAR_WIDTH.length - 2)))).toString()}px)`,
         adminlqTopbarStyle: {
             'height': ADMINLQ_TOPBAR_HEIGHT,
             'z-index': ADMINLQ_TOPBAR_Z_INDEX
         },
-        adminlqBlankStyle: {
-            'width': (window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH)?ADMINLQ_SIDEBAR_WIDTH_HIDDEN:ADMINLQ_SIDEBAR_WIDTH,
+        topbarBlankStyle: {
+            'width': (window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH)?ADMINLQ_SIDEBAR_WIDTH_HIDDEN:ADMINLQ_SIDEBAR_WIDTH,
         },
         antIndentSvgIconClass: {
-            'is-hidden': !(window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH)
+            'is-hidden': !(window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH)
         },
         antOutdentSvgIconClass: {
-            'is-hidden': window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH
+            'is-hidden': window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH
         },
         sidebarStyle: {
             'width': ADMINLQ_SIDEBAR_WIDTH,
             'z-index': ADMINLQ_SIDEBAR_Z_INDEX,
-            'transform': (window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH)?`translateX(-${ADMINLQ_SIDEBAR_WIDTH})`:'translateX(0px)'
+            'transform': (window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH)?`translateX(-${ADMINLQ_SIDEBAR_WIDTH})`:'translateX(0px)'
+        },
+        sidebarHeaderStyle: {
+            'height': ADMINLQ_TOPBAR_HEIGHT
+        },
+        viewBlankStyle: {
+            'width': (window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH)?ADMINLQ_SIDEBAR_WIDTH_HIDDEN:ADMINLQ_SIDEBAR_WIDTH,
+        },
+        footerBlankStyle: {
+            'width': (window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH)?ADMINLQ_SIDEBAR_WIDTH_HIDDEN:ADMINLQ_SIDEBAR_WIDTH,
         },
         maskForSidebarStyle: {
             'z-index': ADMINLQ_MASK_FOR_SIDEBAR_HIDDEN_Z_INDEX
@@ -49,22 +59,26 @@ const adminlqApp = new Vue ({
     },
     methods: {
         topbarSidebarToggle: (event) => {
-            if(window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH){
+            if (window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH) {
                 adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
                 adminlqApp.maskForSidebarStyle['z-index'] = ADMINLQ_MASK_FOR_SIDEBAR_SHOW_Z_INDEX;
                 adminlqApp.antIndentSvgIconClass['is-hidden'] = true;
                 adminlqApp.antOutdentSvgIconClass['is-hidden'] = false;
                 adminlqApp.sidebar_hidden_state = false;
-            }else{
-                if(adminlqApp.sidebar_accordion_state){
-                    adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
+            }else {
+                if (adminlqApp.sidebar_accordion_state) {
+                    adminlqApp.topbarBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
+                    adminlqApp.viewBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
+                    adminlqApp.footerBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
                     adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
                     adminlqApp.antIndentSvgIconClass['is-hidden'] = true;
                     adminlqApp.antOutdentSvgIconClass['is-hidden'] = false;
                     adminlqApp.sidebar_accordion_state = false;
-                }else{
+                }else {
                     adminlqApp.sidebarStyle['transform'] = adminlqApp.sidebar_accordion_translatex;
-                    adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
+                    adminlqApp.topbarBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
+                    adminlqApp.viewBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
+                    adminlqApp.footerBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
                     adminlqApp.antOutdentSvgIconClass['is-hidden'] = true;
                     adminlqApp.antIndentSvgIconClass['is-hidden'] = false;
                     adminlqApp.sidebar_accordion_state = true;
@@ -81,20 +95,26 @@ const adminlqApp = new Vue ({
     }
 });
 window.onresize = () => {
-    if(window.innerWidth <= ADMINLQ_TABLET_MAX_WIDTH){
+    if (window.innerWidth <= ADMINLQ_APP_BREAKPOINT_WIDTH) {
         adminlqApp.sidebarStyle['transform'] = `translateX(-${ADMINLQ_SIDEBAR_WIDTH})`;
-        adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_HIDDEN;
+        adminlqApp.topbarBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_HIDDEN;
+        adminlqApp.viewBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_HIDDEN;
+        adminlqApp.footerBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_HIDDEN;
         adminlqApp.antOutdentSvgIconClass['is-hidden'] = true;
         adminlqApp.antIndentSvgIconClass['is-hidden'] = false;
         adminlqApp.sidebar_hidden_state = true;
-    }else{
-        if(adminlqApp.sidebar_accordion_state){
-            adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
+    }else {
+        if (adminlqApp.sidebar_accordion_state) {
+            adminlqApp.topbarBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
+            adminlqApp.viewBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
+            adminlqApp.footerBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH_ACCORDION;
             adminlqApp.sidebarStyle['transform'] = adminlqApp.sidebar_accordion_translatex;
             adminlqApp.antOutdentSvgIconClass['is-hidden'] = true;
             adminlqApp.antIndentSvgIconClass['is-hidden'] = false;
-        }else{
-            adminlqApp.adminlqBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
+        }else {
+            adminlqApp.topbarBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
+            adminlqApp.viewBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
+            adminlqApp.footerBlankStyle['width'] = ADMINLQ_SIDEBAR_WIDTH;
             adminlqApp.sidebarStyle['transform'] = 'translateX(0px)';
             adminlqApp.antIndentSvgIconClass['is-hidden'] = true;
             adminlqApp.antOutdentSvgIconClass['is-hidden'] = false;
